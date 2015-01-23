1ff00c76635cd9506df0bdbfd0b12f1de720b436
andela-gukpere <godson.ukpere@andela.co>
2015-01-24 00:02:19 +0100

    Modified string-length test case

--- public/exercises/string_length/string-length-tests.js
+++ public/exercises/string_length/string-length-tests.js
@@ -1,5 +1,5 @@
 //We shall contemplate truth by testing reality, via spec expectations.
-describe("String Lengths in arrays ", function() {
+describe("String Lengths returned in arrays ", function() {
 
   describe("Case for a single string", function() {
 
@@ -19,8 +19,8 @@ describe("String Lengths in arrays ", function() {
       expect(stringLength(['Adam','Frankel'])).toEqual([4,7]);
     });
 
-    it("should return [7, 6, 5] for ['Michael', 'Curtis', 'Faith']", function() {
-      expect(stringLength(['Michael', 'Curtis', 'Faith'])).toEqual([7, 6, 5]);
+    it("should return [7, 7, 5] for ['Michael', 'William', 'Smith']", function() {
+      expect(stringLength(['Michael', 'William', 'Smith'])).toEqual([7, 7, 5]);
     });
 
   });
